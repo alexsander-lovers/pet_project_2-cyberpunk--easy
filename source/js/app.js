9138d66a8a1d50bda7affc7bddcf208634cf2512
alexsander-lovers <150514172+alexsander-lovers@users.noreply.github.com>
2026-09-09 15:44:49 +0300

    Размечает и добавляет блок hero + исправляет неточности в других блоках

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,13 @@
 
+const swiperComments = new Swiper('.swiper', {
+  // spaceBetween: 20,
+  slidesPerView: 1,
+  loop: true,
+  autoHeight: true,
+  speed: 1000,
+  grabCursor: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+})
